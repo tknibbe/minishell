de08b2d7d53e10c684d64292652faef410709453
Cris Van Schijndel <cvan-sch@f1r1s13.codam.nl>
2023-06-14 14:58:41 +0200

    ya

--- src/cris_main.c
+++ src/cris_main.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   cris_main.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: tknibbe <tknibbe@student.42.fr>              +#+                     */
+/*   By: cvan-sch <cvan-sch@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/06/01 17:24:49 by tknibbe       #+#    #+#                 */
-/*   Updated: 2023/06/14 14:52:01 by cvan-sch      ########   odam.nl         */
+/*   Created: 2023/06/14 14:53:44 by cvan-sch      #+#    #+#                 */
+/*   Updated: 2023/06/14 14:55:18 by cvan-sch      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../includes/minishell.h"
 #include <stdio.h>
-#include <readline/readline.h>
+#include <unistd.h>
 #include <readline/history.h>
+#include <readline/readline.h>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	printf("minishell/>");
-	readline(NULL);
-
-	return (0);
+	char *string = readline("minishell/>");
+	printf("string: %s\n", string);
 }
