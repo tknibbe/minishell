5ecf2f0f7cff0a79a81eb8bdccf98049ee631e46
Cris Van Schijndel <cvan-sch@f0r2s12.codam.nl>
2023-07-09 18:21:19 +0200

    the redirection loop seems to work which makes me happy

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -43,10 +43,10 @@ t_rdr	*get_some_rdr(void)
 	t_rdr *head;
 
 	head = rdr_new("input1", 1);
-	rdr_addback(&head, rdr_new("output", 2));
-	rdr_addback(&head, rdr_new("output1", 2));
-	rdr_addback(&head, rdr_new("output2", 2));
-	rdr_addback(&head, rdr_new("output3", 2));
+	// rdr_addback(&head, rdr_new("output", 2));
+	// rdr_addback(&head, rdr_new("output1", 2));
+	// rdr_addback(&head, rdr_new("output2", 2));
+	// rdr_addback(&head, rdr_new("output3", 2));
 	return (head);
 }
 
@@ -82,9 +82,15 @@ void	redirection(t_rdr *r)
 	while (r)
 	{
 		if (r->type == 1) // <
+		{
+			printf("type = <\n");
 			open_file_and_dup(r->file, O_RDONLY, STDIN_FILENO);
+		}
 		else if (r->type == 2) // >
+		{
+			printf("type = >\n");
 			open_file_and_dup(r->file, O_WRONLY | O_CREAT | O_TRUNC, STDOUT_FILENO);
+		}
 		r = r->next;
 	}
 }
@@ -92,8 +98,9 @@ void	redirection(t_rdr *r)
 void	execute_command(t_exec *t)
 {
 	redirection(t->rdr);
-	printf("yo\n");
+	write(2, "redirection succesfull?\n", 25);
 	execve(t->cmd[0], t->cmd, NULL);
+	write(2, "execve failed\n", 15);
 	exit(1);
 	//printf("did it work?\n");
 }
@@ -105,7 +112,7 @@ void	test_exec(char **env)
 	if (pid == 0)
 	{
 		t_exec	*t = ft_lstnew();
-		t->cmd = ft_split("program", ' ');
+		t->cmd = ft_split("/bin/cat", ' ');
 		t->env = env;
 		t->rdr = get_some_rdr();
 		execute_command(t);
