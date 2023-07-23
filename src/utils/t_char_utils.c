f682a7b6cc093da2cbc7ba38cab14c2c1c6b3b1c
Cris Van Schijndel <cvan-sch@f1r6s17.codam.nl>
2023-07-23 15:39:59 +0200

    back to work

---
+++ src/utils/t_char_utils.c
@@ -0,0 +1,37 @@
+#include <minishell.h>
+
+t_str	*tstr_new(char *content)
+{
+	t_str	*new;
+
+	new = malloc(sizeof(t_str));
+	if (!new)
+		ft_exit("Malloc error\n", errno);
+	new->next = NULL;
+	new->str = content;
+	return (new);
+}
+
+void	tstr_addback(t_str **head, t_str *new)
+{
+	t_str	*tmp;
+
+	tmp = *head;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+void	tstr_addnext(t_str *current, t_str *new)
+{
+	t_str	*tmp;
+
+	tmp = current->next;
+	current->next = new;
+	new->next = tmp;
+}
