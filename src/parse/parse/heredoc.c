e4fe880c4bfdc658280f59aaa5ea233ce40116e6
tknibbe <tymonknibbe@gmail.com>
2023-07-25 11:43:46 +0200

    fixed backslashes in heredoc

--- src/parse/parse/heredoc.c
+++ src/parse/parse/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:05:32 by tknibbe           #+#    #+#             */
-/*   Updated: 2023/07/24 16:34:17 by tknibbe          ###   ########.fr       */
+/*   Updated: 2023/07/25 11:42:53 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 //i needs to be set to the end of the delimiter
 static char	*get_delimiter(char *input, int *i);
 static int	set_expand(char *delimiter);
-static char	*trim_newline(char *line);
+static char	*trim_char(char *line, char *c);
+static char *backslash_check(char *line);
 
 void	add_heredoc(char *input, t_exec *node, int *i)
 {
@@ -25,22 +26,23 @@ void	add_heredoc(char *input, t_exec *node, int *i)
 
 	delimiter = get_delimiter(input, i);
 	expand = set_expand(delimiter);
-	if (expand == HEREDOC_NO_EXP)
+	if (expand == HEREDOC_NO_EXP) //cleaner?
 		delimiter = ft_strdel(delimiter, "\"\'");
 	line = NULL;
 	while (1)
 	{
 		write(1, ">", 1);
-		line = get_next_line(0);
+		line = get_next_line(STDIN_FILENO);
+		line = backslash_check(line);
 		if (!ft_strncmp(delimiter, line, ft_strlen(delimiter)))
 			break ;
-		line = trim_newline(line);
+		line = trim_char(line, "\n");
 		rdr_lstadd_back(&node->rdr, rdr_lstnew(line, expand));
 	}
 	free(line);
 }
 
-static int	set_expand(char *d)
+static int	set_expand(char *d) // cleaner
 {
 	int		i;
 	int		j;
@@ -75,7 +77,7 @@ static char	*get_delimiter(char *input, int *i)
 	while (whitespace(input[*i]) || input[*i] == '<')
 		*i += 1;
 	start = *i;
-	while (whitespace(input[*i]) == 0 && input[*i])
+	while (!whitespace(input[*i]) && input[*i])
 		*i += 1;
 	temp = ft_substr(input, start, *i - start);
 	if (temp == NULL)
@@ -84,12 +86,29 @@ static char	*get_delimiter(char *input, int *i)
 	free (temp);
 	return (str);
 }
+static char *backslash_check(char *line)
+{
+	char *str;
+	char *temp;
 
-static char	*trim_newline(char *line)
+	if (line[ft_strlen(line) - 2] == '\\')
+	{
+		line = trim_char(line, "\\\n");
+		write(1, ">", 1);
+		str = get_next_line(STDIN_FILENO);
+		temp = ft_strjoin(line, str);
+		free (line);
+		free (str);
+		return (temp);
+	}
+	return (line);
+}
+
+static char	*trim_char(char *line, char *c)
 {
 	char	*ret;
 
-	ret = ft_strtrim(line, "\n");
+	ret = ft_strtrim(line, c);
 	free(line);
 	return (ret);
 }
