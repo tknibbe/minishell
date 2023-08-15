21daee48b9306cdcbb844afcdb5a21e43ea462e2
tknibbe <tymonknibbe@gmail.com>
2023-08-15 15:37:17 +0200

    EYYOEYEYEYO

---
+++ src/parse/split_pipelines/list_utils.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 15:20:01 by tknibbe           #+#    #+#             */
+/*   Updated: 2023/08/10 15:21:30 by tknibbe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+t_list	*t_listnew(void)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		ft_exit("Malloc error", errno);
+	node->token = NULL;
+	node->input = NULL;
+	node->and_or = NULL;
+	node->exec = NULL;
+	node->exit_code = 0;
+	node->next = NULL;
+	return (node);
+}
+
+t_list	*t_listlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != 0)
+		lst = lst->next;
+	return (lst);
+}
+
+void	t_listadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	last = t_listlast(*lst);
+	last->next = new;
+}
---
+++ src/parse/split_pipelines/split_pipelines.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_pipelines.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 14:34:27 by tknibbe           #+#    #+#             */
+/*   Updated: 2023/08/15 14:25:01 by tknibbe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+int	and_or_instr(char *input, int *token, int i)
+{
+	while (input[i])
+	{
+		if (token[i] == AND || token[i] == OR)
+			return (i);
+		i++;
+	}
+	if (!input[i])
+		return (0);
+	return (ft_strlen(input) - 1);
+}
+
+int	*copy_token(int *token, int start, int end)
+{
+	int	*new_token;
+	int	i;
+
+	new_token = malloc(sizeof(int) * end - start);
+	if (!new_token)
+		ft_exit("Malloc error", errno);
+	i = 0;
+	while (start + i < end)
+	{
+		new_token[i] = token[start + i];
+		i++;
+	}
+	return (new_token);
+}
+
+int	split_pipelines(char *input, t_ally *all)
+{
+	t_list	*node;
+	t_list	*data;
+	int		start;
+	int		end;
+
+	start = 0;
+	data = all->list;
+	end = and_or_instr(input, data->token, 0);
+	print_tokens(all->list, ft_strlen(input));
+	if (end == 0)
+		return (0);
+	all->list = NULL;
+	while (input[end])
+	{
+		if (data->token[end] == AND || data->token[end] == OR || !input[end + 1])
+		{
+			if (!input[end + 1])
+				end++;
+			node = t_listnew();
+			t_listadd_back(&all->list, node);
+			node->input = ft_substr(input, start, end - start);
+			node->token = copy_token(data->token, start, end);
+			node->and_or = &input[end];
+			if (input[end + 2])
+				end += 2;
+			start = end;
+		}
+		else
+			end++;
+	}
+	printf("exit split_pipes\n");
+	free(data->token);
+	free(data);
+	return (1);
+}
+ 
