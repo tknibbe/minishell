857845cd7690c6898afed630edfe823ae5e92e77
tknibbe <tymonknibbe@gmail.com>
2023-09-09 15:27:34 +0200

    norm fixes

---
+++ src/parse/parse_main.c
@@ -0,0 +1,27 @@
+
+#include <minishell.h>
+
+t_list	*parse_input(char *input)
+{
+	t_list	*list;
+	t_list	*temp;
+
+	list = t_listnew();
+	list->input = ft_strtrim(input, " ");
+	tokenize(list);
+	if (check_syntax(list))
+		return (NULL);
+	if (split_pipelines(input, &list))
+	{
+		temp = list;
+		while (temp)
+		{
+			parse(temp->input, temp);
+			temp = temp->next;
+		}
+		print_whole_list(list);
+	}
+	else
+		parse(input, list);
+	return (list);
+}
