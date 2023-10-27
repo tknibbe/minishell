7242dda8648163ca21f807e91703b91c2f18526a
Cristhian <cristje@pop-os.localdomain>
2023-10-27 14:26:15 +0200

    yes

--- src/execution/redirect.c
+++ src/execution/redirect.c
@@ -42,18 +42,32 @@ void	heredoc_expand(t_str *heredoc, t_env *e)
 	}
 }
 
-int	do_heredoc_or_so(t_rdr *r, t_env *e, int hierdok_num)
-//needs to be executed
+char	*getfilename(int num)
+{
+	char	*nbr;
+	char	*filename;
+
+	nbr = ft_itoa(num);
+	if (!nbr)
+		ft_minishell_error("malloc()", NULL, strerror(errno), errno);
+	filename = ft_strjoin("/tmp/unieke_naam", nbr);
+	if (!filename)
+		ft_minishell_error("malloc()", NULL, strerror(errno), errno);
+	free(nbr);
+	return (filename);
+}
+
+void	do_heredoc_or_so(t_rdr *r, t_env *e, int hierdok_num, int in)
 {
 	int		fd;
 	t_str	*heredoc;
-	// append hdnum to unieke naam
-	fd = open("/tmp/unieke_naam2", O_CREAT | O_RDWR | O_EXCL, 0644);
+	char	*filename;
+
+	filename = getfilename(hierdok_num);
+	printf("filename: %s\n", filename);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd < 0)
-		ft_minishell_error("open()", "\"/tmp/unieke_naam2\"", \
-			strerror(errno), errno);
-	if (unlink("/tmp/unieke_naam2"))
-		ft_minishell_error("unlink()", NULL, strerror(errno), errno);
+		ft_minishell_error("open()", filename, strerror(errno), errno);
 	heredoc = r->file;
 	if (r->type == HEREDOC)
 		heredoc_expand(heredoc, e);
@@ -62,7 +76,16 @@ int	do_heredoc_or_so(t_rdr *r, t_env *e, int hierdok_num)
 		write(fd, heredoc->str, ft_strlen(heredoc->str));
 		heredoc = heredoc->next;
 	}
-	return (fd);
+	close(fd);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		ft_minishell_error("open()", filename, strerror(errno), errno);
+	if (dup2(fd, in) < 0)
+		ft_minishell_error("dup2()", "duplicating heredoc", strerror(errno), errno);
+	close(fd);
+	if (unlink(filename))
+		ft_minishell_error("unlink()", NULL, strerror(errno), errno);
+	free(filename);
 }
 
 void	open_file_and_dup(char *file, int flag, int to_dup, int permission)
@@ -86,19 +109,19 @@ int	redirect(t_rdr *r, t_env_info *e, int in, int out, int hierdok_num)
 	{
 		if (r->type == REDIRRIGHT || r->type == APPEND)
 			ret = 1;
-		if ((r->type == HEREDOC || r->type == HEREDOC_NO_EXP) && in != -1)
-			in = do_heredoc_or_so(r, e->head, hierdok_num);
+		if ((r->type == HEREDOC || r->type == HEREDOC_NO_EXP))
+			do_heredoc_or_so(r, e->head, hierdok_num, in);
 		else // fix for heredoccie
 		{
 			file = full_expansion(r->file, e);
 			if (!(*file) || (*file && *(file + 1)))
 				ft_minishell_error("ambiguous redirect", "expansion results in multiple or no arguments", NULL, 1);
 			if (r->type == REDIRLEFT && in != -1)
-				open_file_and_dup(*file, O_RDONLY, STDIN_FILENO, 0);
+				open_file_and_dup(*file, O_RDONLY, in, 0);
 			else if (r->type == REDIRRIGHT)
-				open_file_and_dup(*file, O_WRONLY | O_CREAT | O_TRUNC, STDOUT_FILENO, 420);
+				open_file_and_dup(*file, O_WRONLY | O_CREAT | O_TRUNC, out, 420);
 			else if (r->type == APPEND)
-				open_file_and_dup(*file,O_WRONLY | O_CREAT | O_APPEND, STDOUT_FILENO, 420);
+				open_file_and_dup(*file,O_WRONLY | O_CREAT | O_APPEND, out, 420);
 			free_dp(file);
 		}
 		r = r->next;
