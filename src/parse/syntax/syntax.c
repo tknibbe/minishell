ba57e4f7327c283aa4af3fdaae72910637c9015f
Tymon Knibbe <tknibbe@f1r5s5.codam.nl>
2023-11-02 12:10:39 +0100

    fixed multiple redirects in a row

--- src/parse/syntax/syntax.c
+++ src/parse/syntax/syntax.c
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 13:46:32 by tknibbe           #+#    #+#             */
-/*   Updated: 2023/10/21 15:34:49 by tknibbe          ###   ########.fr       */
+/*   Updated: 2023/11/02 12:09:37 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,9 @@ static int	rdr_check(t_list *list, int *i)
 	temp = *i;
 	while (list->token[temp] == list->token[*i] && list->input[*i])
 		*i += 1;
+	printf("%d\n", *i - temp);
+	if ((*i - temp) > 2)
+		return (ft_syntax_error(' ', list->token[*i-1]));
 	while (list->token[*i] == BLANK && list->input[*i])
 		*i += 1;
 	if (!list->input[*i])
