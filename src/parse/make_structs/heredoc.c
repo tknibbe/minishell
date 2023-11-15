4ee3d31ec286e73e479da81cb1cd8fe84bdc5506
Tymon Knibbe <tknibbe@f1r1s13.codam.nl>
2023-11-15 13:04:44 +0100

    fixed expanding when not supposed to in heredoc

--- src/parse/make_structs/heredoc.c
+++ src/parse/make_structs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:05:32 by tknibbe           #+#    #+#             */
-/*   Updated: 2023/11/09 14:58:19 by tknibbe          ###   ########.fr       */
+/*   Updated: 2023/11/15 13:02:49 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,18 @@ static void	heredoc(t_heredoc *doc);
 
 void	init_struct_and_fork(t_heredoc *doc, t_list *list, int *i)
 {
+	char	*swapstring;
+
 	doc->delimiter = get_delimiter(list, i);
-	printf("delimiter = [%s]\n", doc->delimiter);
 	doc->expand = set_expand(doc->delimiter);
+	swapstring = ft_strdel(doc->delimiter, "\'\"()");
+	if(!swapstring)
+		ft_minishell_error("Malloc()", NULL, strerror(errno), errno);
+	free (doc->delimiter);
+	doc->delimiter = ft_strjoin(swapstring, "\n");
+	if (!doc->delimiter)
+		ft_minishell_error("malloc()", NULL, strerror(errno), errno);
+	free(swapstring);
 	doc->line = "";
 	if (pipe(doc->pipefd) < 0)
 		ft_minishell_error("pipe()", "creating the pipe for heredoc", \
@@ -108,8 +117,6 @@ static char	*get_delimiter(t_list *list, int *i)
 {
 	int		start;
 	char	*temp;
-	char	*str;
-
 	while (list->token[*i] == BLANK || list->input[*i] == '<')
 		*i += 1;
 	start = *i;
@@ -120,11 +127,6 @@ static char	*get_delimiter(t_list *list, int *i)
 	temp = ft_substr(list->input, start, *i - start);
 	if (temp == NULL)
 		ft_minishell_error("malloc()", NULL, strerror(errno), errno);
-	str = ft_strdel(temp, "\'\"()");
-	free (temp);
-	temp = ft_strjoin(str, "\n");
-	if (temp == NULL)
-		ft_minishell_error("malloc()", NULL, strerror(errno), errno);
-	free(str);
 	return (temp);
 }
+
